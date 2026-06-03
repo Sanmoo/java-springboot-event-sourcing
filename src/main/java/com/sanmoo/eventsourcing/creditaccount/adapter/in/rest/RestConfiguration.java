814b908584c8a58428a9e0cda09cd7460790076d
Samuel Santos <sanmoo.cs@gmail.com>
2026-06-03 19:28:36 -0300

    refactor: rewire Spring configuration for use cases

--- src/main/java/com/sanmoo/eventsourcing/creditaccount/adapter/in/rest/RestConfiguration.java
+++ src/main/java/com/sanmoo/eventsourcing/creditaccount/adapter/in/rest/RestConfiguration.java
@@ -2,7 +2,7 @@ package com.sanmoo.eventsourcing.creditaccount.adapter.in.rest;
 
 import com.sanmoo.eventsourcing.creditaccount.core.port.EventStorePort;
 import com.sanmoo.eventsourcing.creditaccount.core.port.IdempotencyPort;
-import com.sanmoo.eventsourcing.creditaccount.application.service.CreditAccountCommandService;
+import com.sanmoo.eventsourcing.creditaccount.core.usecase.*;
 import tools.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +11,51 @@ import org.springframework.context.annotation.Configuration;
 public class RestConfiguration {
 
     @Bean
-    public CreditAccountCommandService creditAccountCommandService(
+    public CreditAccountUseCaseSupport creditAccountUseCaseSupport(
             EventStorePort eventStorePort,
             IdempotencyPort idempotencyPort,
             ObjectMapper objectMapper
     ) {
-        return new CreditAccountCommandService(eventStorePort, idempotencyPort, objectMapper);
+        return new CreditAccountUseCaseSupport(eventStorePort, idempotencyPort, objectMapper);
+    }
+
+    @Bean
+    public OpenCreditAccountUseCase openCreditAccountUseCase(CreditAccountUseCaseSupport support) {
+        return new OpenCreditAccountUseCase(support);
+    }
+
+    @Bean
+    public AssignCreditLimitUseCase assignCreditLimitUseCase(CreditAccountUseCaseSupport support) {
+        return new AssignCreditLimitUseCase(support);
+    }
+
+    @Bean
+    public ChangeCreditLimitUseCase changeCreditLimitUseCase(CreditAccountUseCaseSupport support) {
+        return new ChangeCreditLimitUseCase(support);
+    }
+
+    @Bean
+    public AuthorizePurchaseUseCase authorizePurchaseUseCase(CreditAccountUseCaseSupport support) {
+        return new AuthorizePurchaseUseCase(support);
+    }
+
+    @Bean
+    public CapturePurchaseUseCase capturePurchaseUseCase(CreditAccountUseCaseSupport support) {
+        return new CapturePurchaseUseCase(support);
+    }
+
+    @Bean
+    public ReleasePurchaseAuthorizationUseCase releasePurchaseAuthorizationUseCase(CreditAccountUseCaseSupport support) {
+        return new ReleasePurchaseAuthorizationUseCase(support);
+    }
+
+    @Bean
+    public ReceivePaymentUseCase receivePaymentUseCase(CreditAccountUseCaseSupport support) {
+        return new ReceivePaymentUseCase(support);
+    }
+
+    @Bean
+    public GetCreditAccountUseCase getCreditAccountUseCase(CreditAccountUseCaseSupport support) {
+        return new GetCreditAccountUseCase(support);
     }
 }
