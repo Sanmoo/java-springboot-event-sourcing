4e30e417309879dcebb725ffe422456c3976c954
Samuel Santos <sanmoo.cs@gmail.com>
2026-06-09 07:56:38 -0300

    refactor: add dto imports to ChangeCreditLimit, GetCreditAccount, OpenCreditAccount usecases

--- src/main/java/com/sanmoo/eventsourcing/creditaccount/core/usecase/GetCreditAccountUseCase.java
+++ src/main/java/com/sanmoo/eventsourcing/creditaccount/core/usecase/GetCreditAccountUseCase.java
@@ -1,5 +1,8 @@
 package com.sanmoo.eventsourcing.creditaccount.core.usecase;
 
+import com.sanmoo.eventsourcing.creditaccount.core.usecase.dto.CreditAccountOutput;
+import com.sanmoo.eventsourcing.creditaccount.core.usecase.dto.GetCreditAccountInput;
+import com.sanmoo.eventsourcing.creditaccount.core.usecase.dto.GetCreditAccountOutput;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
