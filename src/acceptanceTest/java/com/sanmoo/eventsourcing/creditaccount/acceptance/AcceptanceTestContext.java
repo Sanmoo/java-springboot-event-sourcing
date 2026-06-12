281f14557c46baee748cd65b2e668b8267a17844
Samuel Santos <sanmoo.cs@gmail.com>
2026-06-12 04:56:48 -0300

    test(acceptance): extend context with lastResponse and lastIdempotencyKey

--- src/acceptanceTest/java/com/sanmoo/eventsourcing/creditaccount/acceptance/AcceptanceTestContext.java
+++ src/acceptanceTest/java/com/sanmoo/eventsourcing/creditaccount/acceptance/AcceptanceTestContext.java
@@ -1,7 +1,9 @@
 package com.sanmoo.eventsourcing.creditaccount.acceptance;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
 import java.util.UUID;
 
 @Component
@@ -10,6 +12,8 @@ public class AcceptanceTestContext {
     private UUID creditAccountId;
     private UUID authorizationId;
     private Long lastProjectedVersion;
+    private ResponseEntity<Map> lastResponse;
+    private String lastIdempotencyKey;
 
     public UUID getCreditAccountId() { return creditAccountId; }
     public void setCreditAccountId(UUID creditAccountId) { this.creditAccountId = creditAccountId; }
@@ -19,4 +23,10 @@ public class AcceptanceTestContext {
 
     public Long getLastProjectedVersion() { return lastProjectedVersion; }
     public void setLastProjectedVersion(Long lastProjectedVersion) { this.lastProjectedVersion = lastProjectedVersion; }
+
+    public ResponseEntity<Map> getLastResponse() { return lastResponse; }
+    public void setLastResponse(ResponseEntity<Map> lastResponse) { this.lastResponse = lastResponse; }
+
+    public String getLastIdempotencyKey() { return lastIdempotencyKey; }
+    public void setLastIdempotencyKey(String lastIdempotencyKey) { this.lastIdempotencyKey = lastIdempotencyKey; }
 }
